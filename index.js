a5c3641ef687196d2696663f3e42d0858403dd28
Apoorv Singh <Apoorv-Lagwal@live.nmit.ac.nz>
2020-03-10 10:02:28 +1300

    hello

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log(1 + 2, 3 + 4);
+let x = (1 + 2, 3 + 4);
+console.log(x);
+//comma operator
+
+let a = 5
+let b = 10
+if (a == b) {
+console.log("same")
+} else {
+    console.log('not same');
+
+
+    
+    
+}
